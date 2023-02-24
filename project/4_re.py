474d58ea9607eabc23a8e8a07f08fcf97041d74a
dusvlf5950 <dusvlf5950@naver.com>
2023-02-24 12:45:07 +0900

    a

--- project/4_re.py
+++ project/4_re.py
@@ -2,9 +2,11 @@ import re
 
 
 p = re.compile('ca.e') 
-# . : 하나의 문자를 의미 
-#  ^ : 문자열의 시작
-# $ : 문자열의 끝 
+
+# . : 하나의 문자를 의미    (ca.e)---ex) care, cafe, case
+#  ^ : 문자열의 시작    (^de)---ex) desk, destination
+# $ : 문자열의 끝   (se&) : case, base
+
 # print(m.group()) # 매치되지 않으면 에러가 발생
 
     
@@ -14,5 +16,5 @@ def print_match(m):
     else:
         print('매칭되지 않았습니다')
     
-m = p.match('good care')
+m = p.match('careless') # 주어진 문자열의 처음부터 일치하는지 확인
 print_match(m)
---
+++ project/4_re2.py
@@ -0,0 +1,37 @@
+import re
+
+
+p = re.compile('ca.e') 
+
+def print_match(m):
+    if m:
+        print('m.group():',m.group()) # 일치하는 문자열 반환
+        print('m.string:',m.string) # 입력받은 문자열 (괄호 않넣음)
+        print('m.start():',m.start()) # 일치하는 문자열의 시작 index 
+        print('m.end():',m.end()) # 일치하는 문자열의 끝 index
+        print('m.span():',m.span()) # 일치하는 문자열의 시작 / 끝 index
+        
+    else:
+        print('매칭되지 않았습니다')
+    
+    
+#m = p.search('careless') 
+#search : 주여진 문자열중에 일치하는게 있는지 확인
+# print_match(m)
+
+lst = p.findall('good care cafe') 
+# findall : 일치하는 모든 것을 리스트 형태로 변환
+
+print(lst)
+
+
+
+# 1. p = re.compile('원하는 형태')
+# 2. m = p.match('비교할 문자열') : 주어진 문자열의 처음부터 일치하는지 확인
+# 3. m = p.search('비교할 문자열') : 주어진 문자열 중에 일치하는게 있는지 확인
+# 4. lst = p.search('비교할 문자열') : 일치하는 모든 것을 리스트 형태로 변환
+
+# 원하는 형태 : 정규식
+# . (ca.e) : 하나의 문자를 의미
+# ^ (^de) : 문자열의 시작
+# $ (se$) : 문자열의 끝 
